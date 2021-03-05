e6eddd94ab8470820d31c6808963f9cbff36e6c5
Phil <philipp.noller@outlook.de>
2021-03-05 22:10:04 +0100

    added pwa

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ function bottledBeer(){
 function fruitBeer(){
     
 }
+
+window.addEventListener("load", () => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("sw.js");
+    }
+  });
